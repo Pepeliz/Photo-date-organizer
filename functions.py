c15108e15a42b1853e1987e2c2fbecbbc004b6c4
Pepeliz <jppepeliz@gmail.com>
2024-01-11 15:30:46 -0300

    PNG, HEIC, HEIF, MP4, MOV support
    addition of new libraries including pillow_heif and moviepy to support other types of files

--- functions.py
+++ functions.py
@@ -1,24 +1,64 @@
-from os import walk, path
+from os import walk, path, makedirs
 from PIL import Image
+from pillow_heif import register_heif_opener
+from moviepy.editor import VideoFileClip
 from PIL.ExifTags import TAGS
 from shutil import move
 
 def Number_Of_Files(path_file):
     quantity = 0
+
     for _, _, files in walk(path_file):
         quantity += len(files)
+    
     return quantity
 
 
-def File_Date(image_path):
+def File_Date(image_path, file_type):
+    exif_date = None
+
     try:
-        with Image.open(image_path) as image:
-            exif_date = image._getexif()
-            if exif_date:
-                for tag, value in exif_date.items():
-                    tag_name = TAGS.get(tag, tag)
-                    if tag_name == 'DateTimeOriginal':
-                        return value
-    except (IOError, AttributeError):
-        pass
+        if file_type.lower() in [".jpg", ".jpeg"]:
+            with Image.open(image_path) as image:
+                exif_date = image._getexif()
+
+        elif file_type.lower() in [".heif", ".heic"]:
+            with Image.open(image_path) as image:
+                exif_data = image.info.get('exif', b'')
+                if isinstance(exif_data, bytes):
+                    exif_data = exif_data.decode('utf-8')
+                exif_date = exif_data
+                
+        elif file_type.lower() in [".mov", ".mp4"]:
+            with VideoFileClip(image_path) as video:
+                metadata = video.reader.metadata
+                if 'creation_time' in metadata:
+                    exif_date = metadata['creation_time']
+
+        elif file_type.lower() == ".png":
+            with Image.open(image_path) as image:
+                exif_data = image.info.get('png', {})
+                if 'creation_time' in exif_data:
+                    exif_date = exif_data['creation_time']
+
+        if exif_date:
+            for tag, value in exif_date.items():
+                tag_name = TAGS.get(tag, tag)
+                
+                if tag_name == 'DateTimeOriginal':
+                    return value
+                
+    except (IOError, AttributeError, Exception) as e:
+        print(f"Error processing {image_path}: {e}")
+
     return None
+
+
+def Organize_Other_Files(destiny_folder, file_path, file):
+    other_files = path.join(destiny_folder, "Other_files")
+
+    if not path.exists(other_files):
+        makedirs(other_files)
+
+    new_file_path = path.join(other_files, file)
+    move(file_path, new_file_path)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
-from functions import Number_Of_Files, File_Date
-from os import makedirs, path, rmdir, walk
+from functions import Number_Of_Files, File_Date, Organize_Other_Files
+from os import makedirs, path, walk
 from shutil import move
 from datetime import datetime
 
@@ -23,49 +23,49 @@ else:
     print(f"The folder '{final_folder}' already exists.")
 
 for root, _, files in walk(selected_folder):
+
     for file in files:
         file_path = path.join(root, file)
 
         if path.isfile(file_path):
-            _ , extension = path.splitext(file_path)
+            _, extension = path.splitext(file_path)
 
-            if extension.lower() in [".jpg", ".jpeg"]:
-                image_date = File_Date(file_path)
+            if extension.lower() in [".jpg", ".jpeg", ".heic", ".heif"]:
+                image_date = File_Date(file_path, extension)
 
                 if image_date is not None:
                     shoot_date = datetime.strptime(image_date, "%Y:%m:%d %H:%M:%S")
+
                     year = str(shoot_date.year)
                     month = str(shoot_date.month).zfill(2)
                     day = str(shoot_date.day).zfill(2)
+                    hour = str(shoot_date.hour).zfill(2)
+                    minute = str(shoot_date.minute).zfill(2)
+                    second = str(shoot_date.second).zfill(2)
+
                     file_destiny = path.join(final_folder, year, f"Month_{month}")
 
                     if not path.exists(file_destiny):
                         makedirs(file_destiny)
-                    new_file_name = f"Day_{day}{extension}"
+
+                    new_file_name = f"Day_{day}_{hour}_{minute}_{second}{extension}"
                     new_file_path = path.join(file_destiny, new_file_name)
                     move(file_path, new_file_path)
 
                 else:
-                    other_files = path.join(final_folder, "Other_files")
-
-                    if not path.exists(other_files):
-                        makedirs(other_files)
-                    new_file_path = path.join(other_files, file)
-                    move(file_path, new_file_path)
+                    Organize_Other_Files(final_folder, file_path, file)
 
             else:
-                other_files = path.join(final_folder, "Other_files")
+                Organize_Other_Files(final_folder, file_path, file)
+                
+other_files_qty = Number_Of_Files(path.join(final_folder, "Other_files"))
 
-                if not path.exists(other_files):
-                    makedirs(other_files)
-                new_file_path = path.join(other_files, file)
-                move(file_path, new_file_path)
-
-final_folder_qty = Number_Of_Files(final_folder)
+final_folder_qty = Number_Of_Files(final_folder) - other_files_qty
 
 print(f"The number of files organized is {final_folder_qty}")
 
 if final_folder_qty == select_folder_qty:
     print("All the files have been copied correctly")
+
 else:
-    print(f"The number of files without date is {Number_Of_Files(other_files)}")
+    print(f"The number of files without date is {other_files_qty}")
